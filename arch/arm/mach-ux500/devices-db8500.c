24f278ccc82415ddd3a0631d041032b97ba0a2a3
hastalafiesta- <mauroscianca98@gmail.com>
2015-07-25 11:10:03 +0200

    Delete useless cpu steps
    Change-Id: Ica59bd856163cf3e8dc6cdcadf7e416a1ae2fe0e

--- arch/arm/mach-ux500/devices-db8500.c
+++ arch/arm/mach-ux500/devices-db8500.c
@@ -691,26 +691,6 @@ static struct cpufreq_frequency_table db8500_freq_table[] = {
 		.index = 15,
 		.frequency = CPUFREQ_TABLE_END,
 	},
-	[16] = {
-		.index = 16,
-		.frequency = CPUFREQ_TABLE_END,
-	},
-	[17] = {
-		.index = 17,
-		.frequency = CPUFREQ_TABLE_END,
-	},
-	[18] = {
-		.index = 18,
-		.frequency = CPUFREQ_TABLE_END,
-	},
-	[19] = {
-		.index = 19,
-		.frequency = CPUFREQ_TABLE_END,
-	},
-	[20] = {
-		.index = 20,
-		.frequency = CPUFREQ_TABLE_END,
-	},
 #endif
 };
 struct platform_device db8500_prcmu_device = {
